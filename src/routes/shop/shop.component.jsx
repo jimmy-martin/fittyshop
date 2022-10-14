defc1f51066adb5e8f99711aa56f9f9d533e95de
jimmy-martin <jimmy.martin952@gmail.com>
2022-10-14 16:25:17 +0200

    created all categories' pages and fixing some css issues

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,25 +1,15 @@
-import { Fragment, useContext } from 'react';
-import ProductCard from '../../components/product-card/product-card.component';
-import { CategoriesContext } from '../../contexts/categories.context';
+import { Route, Routes } from 'react-router-dom';
 
 import './shop.styles.scss';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={categoriesMap[title]}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => {
-              return <ProductCard key={product.id} product={product} />;
-            })}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <Routes>
+      <Route index element={<CategoriesPreview />}></Route>
+      <Route path=":category" element={<Category />}></Route>
+    </Routes>
   );
 };
 
